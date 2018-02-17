a9a01316659745aa1a11280041bad4f23fdd8013
Poy Chang <poypost@gmail.com>
2018-02-17 14:02:10 +0800

    建立使用 Sql Server 做為資料來源的 DemoApp 專案

---
+++ DemoApp.SqlServer/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using DbManager.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DemoApp.SqlServer
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var options = new DbContextOptionsBuilder<MyDbContext>()
+                .UseSqlServer(configuration["DbManagerOptions:ConnectionString"], b => b.MigrationsAssembly("DemoApp.SqlServer"))
+                .Options;
+
+            return new MyDbContext(options);
+        }
+    }
+}
---
+++ DemoApp.SqlServer/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using DbManager.Interface;
+using DbManager.Schema;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace DemoApp.SqlServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.AddSqlServerDbManager(options =>
+            {
+                options.ConnectionString = Configuration["DbManagerOptions:ConnectionString"];
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IDbManager dbManager)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                var repo = dbManager.Repository<MyTableSet>();
+                repo.Create(new MyTableSet(){ Description = "Hello World!", CreateDate = DateTime.Now});
+                repo.SaveChanges();
+
+                var content = JsonConvert.SerializeObject(repo.Read());
+                await next.Invoke();
+                await context.Response.WriteAsync(content);
+            });
+        }
+    }
+}
